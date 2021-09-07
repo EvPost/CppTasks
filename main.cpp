119fe30d3cb32fd2916c20c8c57a0cf91e3d0fe3
EvPost <90252637+EvPost@users.noreply.github.com>
2021-09-07 21:48:02 +0300

    C++
    Classwork1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 //
 //  main.cpp
-//  pr4
+//  pr5
 //
 //  Created by Air on 06.09.2021.
 //
 
 #include <iostream>
-#include <cmath>
 #include <stdio.h>
-int main(){
-    float m1, m2, r, f1, F;
- std::cout<<"m1=";
-    std::cin>>m1;
-    std::cout<<"m2=";
-       std::cin>>m2;
-    std::cout<<"r=";
-       std::cin>>r;
-    
-    f1=6.673*10e-12;
-    F=f1*m1*m2/(r*r);
-    printf ("F=%f(%f*%f)/%f=%f", f1,m1,m2,r,F);
-}
 
+int main() {
+    // insert code here...
+    float x,y,z,l,q,q1,w,w1;
+    std::cout<<"x=";
+       std::cin>>x;
+    y=x*x;
+    z=y*y;
+    l=z*z;
+    q=l*l;
+    q1=q/x;
+    printf ("%f       ",q1);
+    
+    w=q*q;
+    w1=w*y;
+    printf ("%f",w1);
+    return 0;
+}
