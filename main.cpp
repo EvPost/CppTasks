5b375df2cccf0003aa90d760a3122c13d3356457
EvPost <90252637+EvPost@users.noreply.github.com>
2021-09-13 14:27:34 +0300

    CW2
    Postulga Evgen

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 //
 //  main.cpp
-//  pr4
+//  CW2.02
 //
-//  Created by Air on 06.09.2021.
+//  Created by Air on 11.09.2021.
 //
 
 #include <iostream>
-#include <cmath>
 #include <stdio.h>
-int main(){
-    float m1, m2, r, f1, F;
- std::cout<<"m1=";
-    std::cin>>m1;
-    std::cout<<"m2=";
-       std::cin>>m2;
-    std::cout<<"r=";
-       std::cin>>r;
-    
-    f1=6.673*10e-12;
-    F=f1*m1*m2/(r*r);
-    printf ("F=%f(%f*%f)/%f=%f", f1,m1,m2,r,F);
+#include <math.h>
+
+int main() {
+    float a,b;
+    printf("a=\n");
+    scanf("%f", &a);
+    printf("b=\n");
+    scanf("%f", &b);
+    printf("c=%f",sqrt(a*a+b*b));
+    return 0;
 }
 
