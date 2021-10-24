852755c7f39405f69e09d954462f7a3ef0efc8ac
EvPost <90252637+EvPost@users.noreply.github.com>
2021-10-24 13:15:06 +0300

    CW9 Postulga Kompmat1
    Postulga Evgen Kompmat1
    CW9 (7.1-7.6)

---
+++ main7_05.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  CW9_7.05
+//
+//  Created by Air on 21.10.2021.
+//
+
+#include <iostream>
+
+#define N 5
+
+int main(){
+    int mas[N];
+    for (int i=0; i<N; i++){
+        printf("a[%d]=", i);
+        scanf("%d", &mas[i]);
+    }
+    int par=0, nepar=0;
+    for (int i=0; i<N; i++){
+        if(i%2==0) par=par+mas[i];
+        else nepar=nepar+mas[i];
+    }
+    printf("par=%d\nnepar=%d",par, nepar);
+}
